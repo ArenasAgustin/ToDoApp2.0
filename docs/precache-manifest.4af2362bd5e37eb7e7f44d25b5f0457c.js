a468a172e823634728e0e7c1f84b19bb21cdc6d0
“ArenasAgustin” <arenasagustin7@gmail.com>
2021-10-31 20:59:18 -0300

    gh-pages

---
+++ docs/precache-manifest.4af2362bd5e37eb7e7f44d25b5f0457c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c23295bc93d02a0f2dcd6fe0fbdf5030",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52305f7461b2b3e578ee",
+    "url": "/static/css/main.2ddbbd24.chunk.css"
+  },
+  {
+    "revision": "a068cbaacc83a8d2c4fe",
+    "url": "/static/js/2.c61cbcca.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.c61cbcca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52305f7461b2b3e578ee",
+    "url": "/static/js/main.be7f5931.chunk.js"
+  },
+  {
+    "revision": "8eaaebe7465f69e4c1db",
+    "url": "/static/js/runtime-main.4f71040b.js"
+  }
+]);
